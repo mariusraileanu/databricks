e58a52e596670c640f5e33dd9cb40665b3ede23d
mraileanu@microsoft.com <mraileanu@microsoft.com>
2022-04-21 16:18:23 +0000

    silver bug fix

--- ETL Framework/operators/bronze_to_silver.scala
+++ ETL Framework/operators/bronze_to_silver.scala
@@ -14,24 +14,24 @@ dbutils.widgets.text("bronzeContainer", "")
 
 // COMMAND ----------
 
-def createSilverTable(bronzeTableName: String, silverFilePath: String, fileName: String, fileType: String, tableName: String, silverContainer: String, bronzeContainer: String) = {
+case class clsPipelineTransformation(SRC_COLUMN:String, TGT_COLUMN:String, DATA_TYPE:String, TRANSFORMATION:String)
+
+def createSilverTable(bronzeTableName: String, silverFilePath: String, fileName: String, fileType: String, silverTableName: String, silverContainer: String, bronzeContainer: String, pipelineName: String) = {
     var connectionProperties = new connectToSQL(jdbcKey = "metadataSQLJDBC", usernameKey = "metadataSQLDBUsername", passwordKey = "metadataSQLDBPassword")
     val pipelineTransformationDF = spark.read.jdbc(connectionProperties.jdbcUrl, "V_PIPELINE_TRANSFORMATION", connectionProperties.connectionProperties).filter(s"PIPELINE_NAME = '$pipelineName'")
 
     val newColumns = LinkedHashMap[String, String]()
-
-    case class clsPipelineTransformation(SRC_COLUMN:String, TGT_COLUMN:String, DATA_TYPE:String, TRANSFORMATION:String)
     pipelineTransformationDF.as[clsPipelineTransformation].take(pipelineTransformationDF.count.toInt).foreach(row => newColumns(s"${row.SRC_COLUMN}") = s"${row.TGT_COLUMN}")
   
     val silverFullFilePath = f"/mnt/$silverContainer/$silverFilePath/$fileName.$fileType"
     println(f"Loading data from $bronzeContainer.$bronzeTableName")    
-    val df = spark.table("$bronzeContainer.$bronzeTableName").transform(renameColumns(newColumns))
+    val df = spark.table(f"$bronzeContainer.$bronzeTableName").transform(renameColumns(newColumns))
     
     println(f"Saving data to $silverFullFilePath")
     df.write.format("delta").mode("overwrite").save(silverFullFilePath)
   
     // Create the table.
-    spark.sql(f"CREATE TABLE IF NOT EXISTS $silverContainer.$tableName USING DELTA LOCATION '$silverFullFilePath'")
+    spark.sql(f"CREATE TABLE IF NOT EXISTS $silverContainer.$silverTableName USING DELTA LOCATION '$silverFullFilePath'")
 }
 
 // COMMAND ----------
@@ -45,4 +45,4 @@ val silverFilePath = dbutils.widgets.get("silverFilePath")
 val silverContainer = dbutils.widgets.get("silverContainer")
 val bronzeContainer = dbutils.widgets.get("bronzeContainer")
 
-createSilverTable(bronzeTableName = bronzeTableName, silverFilePath = silverFilePath, fileName = fileName, fileType = fileType, tableName = tableName, silverContainer = silverContainer, bronzeContainer = bronzeContainer)
+createSilverTable(bronzeTableName = bronzeTableName, silverFilePath = silverFilePath, fileName = fileName, fileType = fileType, silverTableName = silverTableName, silverContainer = silverContainer, bronzeContainer = bronzeContainer, pipelineName = pipelineName)
